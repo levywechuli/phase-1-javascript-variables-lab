8e77f4cb3dcff83797816b0adecc149dc41ddc8a
Levy Wechuli <levy.wechuli@student.moringaschool.com>
2024-09-23 12:54:23 +0300

    adding function

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Code your solution in this file!
+
+
+const companyName = `Scuber`;
+
+
+
+let mostProfitableNeighborhood = 'Chelsea';
+
+function calculateProfit() {
+    console.log(mostProfitableNeighborhood);
+}
+
+calculateProfit();
+
+let companyCeo = `Susan Smith`;
+
+
+function goCeo(){
+    console.log(companyCeo);
+}
+
+goCeo();
